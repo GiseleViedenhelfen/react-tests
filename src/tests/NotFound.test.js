75d675166f79b23de592617c80ec2c86ed223117
Gisele <gisele.viedenhelfen@gmail.com>
2022-05-09 19:02:15 -0300

    testa página not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import NotFound from '../components/NotFound';
+
+describe('testa o componente "not found"', () => {
+  test('', () => {
+    const historicoDeNavegacao = createMemoryHistory();
+    render(
+      <Router history={ historicoDeNavegacao }>
+        <NotFound />
+      </Router>,
+    );
+    historicoDeNavegacao.push('/qualquer-rota');
+    const notFound = screen.getByRole('heading',
+      { name: 'Page requested not found Crying emoji', level: 2 });
+    expect(notFound).toBeInTheDocument();
+  });
+  test('testa se a imagem de url "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    () => {
+      const historicoDeNavegacao = createMemoryHistory();
+      render(
+        <Router history={ historicoDeNavegacao }>
+          <NotFound />
+        </Router>,
+      );
+      historicoDeNavegacao.push('/qualquer-rota');
+      const imgRenderizada = screen.getByRole('img',
+        { name: 'Pikachu crying because the page requested was not found' });
+      expect(imgRenderizada).toBeInTheDocument();
+      const src = screen.getByAltText(/Pikachu crying /);
+      expect(src).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    });
+});
